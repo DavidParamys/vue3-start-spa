4d135ececb0774add6d487e2f7894c1331aafa74
DavidDaiNS <daviddai1996.working@gmail.com>
2024-07-18 15:40:52 +0800

    + Overall upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <navbar 
+        :pages="pages"
+        :active-page="activePage"
+        :nav-link-click="(index) => activePage = index"
+    ></navbar>
+
+    <page-viewer 
+        :page="pages[activePage]">
+    </page-viewer>
+
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+import PageViewer from './components/PageViewer.vue'
+
+export default {
+    components:{
+        Navbar,
+        PageViewer
+    },
+    data() {
+        return {
+            activePage: 0,
+            pages: this.getPages()
+        }
+    },
+    methods: {
+        async getPages() {
+            let res = await fetch('pages.json');
+            let data = await res.json();
+            console.log(data)
+            return data;
+        }
+    }
+}
+</script>
