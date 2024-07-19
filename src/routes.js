551c8d3f6cd4e8de4f3a7b126218dd17e427855a
DavidDaiNS <daviddai1996.working@gmail.com>
2024-07-19 16:07:55 +0800

    + Add routes + Finish 6.1 Introducing Vue Router

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+import PageViewer from './components/PageViewer.vue'
+import CreatePage from './components/CreatePage.vue'
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: PageViewer
+        },
+        {
+            path: '/create',
+            name: 'Create',
+            component: CreatePage
+        }
+    ]
+})
+
+export default router
