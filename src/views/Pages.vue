f981e59488b8bf806e32d614ad1469a1b026402a
DavidDaiNS <daviddai1996.working@gmail.com>
2024-07-22 10:03:11 +0800

    + Finish 7.1 Introducing the Composition API

--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -1,6 +1,26 @@
 <template>
-    <div class="container">
-        <h2>Page Management</h2>
-        <router-view></router-view>
-    </div>
+    <p>{{data.counter}}</p>
+    <button @click.prevent="click">click</button>
 </template>
+<script setup>
+import {ref, reactive} from 'vue';
+
+const data = reactive({counter: 0});
+
+function click() {
+    data.counter++;
+}
+
+// export default {
+//     data(){
+//         return {
+//             counter: 0
+//         };
+//     },
+//     methods: {
+//         click() {
+//             this.counter++;
+//         }
+//     }
+// }
+</script>
